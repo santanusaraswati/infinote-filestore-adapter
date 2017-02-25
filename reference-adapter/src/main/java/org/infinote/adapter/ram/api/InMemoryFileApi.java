6be3e4a97ee25b515ff74f5fef3c5696031903e9
Santanu Saraswati <saraswati.santanu@gmail.com>
2017-02-26 03:13:11 +0530

    added logback and some more implemetation to serve files etc

--- reference-adapter/src/main/java/org/infinote/adapter/ram/api/InMemoryFileApi.java
+++ reference-adapter/src/main/java/org/infinote/adapter/ram/api/InMemoryFileApi.java
@@ -7,6 +7,9 @@ import org.infinote.adapter.ram.model.FileSystem;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author santanu
@@ -15,11 +18,27 @@ import java.io.InputStream;
 public class InMemoryFileApi implements FileApi {
     @Override
     public FileObject getFile(String id) {
-        return FileSystem.getInstance().getFile(id).map(FileEntity::toFile).orElse(null);
+        return getFileObject(id).orElse(null);
     }
 
     @Override
     public InputStream getFileContent(String id) {
         return FileSystem.getInstance().getFileContent(id).orElse(null);
+    }
+
+    @Override
+    public List<FileObject> getVersions(String id) {
+        // as FileSystem does not support version we just have the latest version available to serve
+        return getFileObject(id).map(f -> Collections.singletonList(f)).orElse(Collections.emptyList());
+    }
+
+    @Override
+    public InputStream getFileContent(String id, String version) {
+        // version is not supported by file systems, so we just get whatever content available ignoring the version
+        return getFileContent(id);
+    }
+
+    private Optional<FileObject> getFileObject(String id) {
+        return FileSystem.getInstance().getFile(id).map(FileEntity::toFile);
     }
 }
